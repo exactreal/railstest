ec300e899acbb4a4f97290a42ed4425efc6bc560
exactreal <exactreal@gmail.com>
2010-10-27 14:42:05 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstest::Application.config.secret_token = 'a9fac70a77ae6b276915c624f284050bd23fad273b9f3b879a4487f6b4c0f271b7295ae2dc6e8c91af2182fe158327b9296d471e6f6686230886264d0aefe6d0'
